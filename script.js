a3ffb8790089a49647aff4d10fd470ba34489d75
Suresh <sureshkumawat26601@gmail.com>
2021-06-20 14:17:12 +0530

    project completed and ready for submition

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+function onChange(v){
+    selrate.innerHTML= v+"%";
+}
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    let myRate=Number(document.getElementById("myRange").value);
+    let p = Number(document.getElementById("principal").value);
+    let years = Number(document.getElementById("years").value);
+    let receiveAmount= ((myRate*p*years)/100).toFixed(2);
+    showintr.innerHTML=`<p>If you deposit <span style="background-color:yellow;">${p}</span>,</p><p>at an Interest rate of <span style="background-color:yellow;">${myRate}%</span>.</p><p>You will recevie an amount of <span style="background-color:yellow;">${receiveAmount}</span>,</p><p>in the year <span style="background-color:yellow;">${2020+years}</span>.</p>`
 }
         
