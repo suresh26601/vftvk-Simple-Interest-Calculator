02ee3db5e09f23a72dd9f5a349ee04c742544a2b
Suresh <sureshkumawat26601@gmail.com>
2021-06-20 15:31:16 +0530

    final commit

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-function onChange(v){
-    selrate.innerHTML= v+"%";
+function onChange(v) {
+    selrate.innerHTML = v + "%";
 }
-function compute()
-{
-    let myRate=Number(document.getElementById("myRange").value);
+function compute() {
     let p = Number(document.getElementById("principal").value);
+    let myRate = Number(document.getElementById("myRange").value);
     let years = Number(document.getElementById("years").value);
-    let receiveAmount= ((myRate*p*years)/100).toFixed(2);
-    showintr.innerHTML=`<p>If you deposit <span style="background-color:yellow;">${p}</span>,</p><p>at an Interest rate of <span style="background-color:yellow;">${myRate}%</span>.</p><p>You will recevie an amount of <span style="background-color:yellow;">${receiveAmount}</span>,</p><p>in the year <span style="background-color:yellow;">${2020+years}</span>.</p>`
+    if (p < 1) {
+        alert("Enter a positive number");
+    }
+    else {
+        let receiveAmount = ((myRate * p * years) / 100).toFixed(2);
+        showintr.innerHTML = `<p>If you deposit <span style="background-color:yellow;">${p}</span>,</p><p>at an Interest rate of <span style="background-color:yellow;">${myRate}%</span>.</p><p>You will recevie an amount of <span style="background-color:yellow;">${receiveAmount}</span>,</p><p>in the year <span style="background-color:yellow;">${new Date().getFullYear() + years}</span>.</p>`
+    }
 }
-        
